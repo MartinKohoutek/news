4619819db203d7f265af29c9ef71b81fd44decc9
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-11-12 10:45:37 +0100

    Seeds Users Table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,10 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'phone' => fake()->phoneNumber(),
+            'role' => fake()->randomElement(['admin', 'user']),
+            'status' => fake()->randomElement(['active', 'inactive']),
+            'photo' => fake()->imageUrl('80', '80'),
             'remember_token' => Str::random(10),
         ];
     }
